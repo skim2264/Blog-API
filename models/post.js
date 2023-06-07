04f5dc0a96387d67ca2fb136b612cb24e813edc5
Soojin Kim <soojinkk98@gmail.com>
2023-06-08 00:55:02 +0900

    images removed

--- models/post.js
+++ models/post.js
@@ -6,7 +6,6 @@ const PostSchema = new Schema({
   title: {type: String, required: true},
   post_text: {type: String, required: true},
   author: {type: Schema.Types.ObjectId, ref: "User", required: true},
-  image: {type: String},
   comments: [{type: Schema.Types.ObjectId, ref: "Comment"}],
   isPrivate: {type: Boolean, default: false}
 },{timestamps: true});
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path");
+
+//multer
+const multer  = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, "uploads")
+  },
+  filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+});
+const upload = multer({storage: storage});
+
 const postController = require("../controllers/postController");
 
 //GET all posts
 router.get('/', postController.all_posts_get);
 
 //POST create post
-router.post("/create", postController.post_create_post);
+router.post('/create', upload.single("image"), postController.post_create_post);
 
 //GET single post 
 router.get('/:postId', postController.post_get);
